c9edd7bc130cbcbd91a70ca9d3120557119b0ff3
TimelordUK <sjames8888@gmail.com>
2025-11-16 19:44:06 +0000

    add cursor size in x11 settings

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Force X11 backend for WSL
 unset WAYLAND_DISPLAY
 export WINIT_UNIX_BACKEND=x11
+export XCURSOR_SIZE=16
 # Set RUST_LOG=debug for verbose logging, or RUST_LOG=info for less verbose
 export RUST_LOG="${RUST_LOG:-info}"
 ./target/release/vis-grep "$@"
